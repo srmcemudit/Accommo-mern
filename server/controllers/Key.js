c8761a0b49eecb11019719b10eaae785fff7b24e
srmcemudit <tudrishu@gmail.com>
2025-05-11 17:13:43 +0530

    GET  KEY

--- server/controllers/Key.js
+++ server/controllers/Key.js
@@ -1,6 +1,6 @@
 const getKey = (req,res) =>{
     try {
-        res.status(200).json({key:process.env.key_id})
+        res.status(200).json({key:process.env.KEY_ID})
     } catch (error) {
         res.status(401).json(error)
     }
